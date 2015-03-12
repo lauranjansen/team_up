d4fa7c5f5739d598c0f95ae9f7cf797717e03cc9
saqibddb <saqib.dadabhoy@gmail.com>
2015-03-12 16:40:47 -0400

    Fixed roles issue on controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,13 @@ class UsersController < ApplicationController
 	end
 
 	def create
-		@user = User.new(user_params)
+		@user = User.new(user_params.except(:roles_attributes))
+
+		user_params[:roles_attributes].each do |id, role_attrs|
+			role = Role.find_by(id: role_attrs['id'])
+			@user.roles << role
+		end
+
 		if @user.save
 			session[:user_id] = @user.id
 			redirect_to user_path(@user), notice: "Welcome to Team Up!"
@@ -22,7 +28,29 @@ class UsersController < ApplicationController
 
 	private
 	def user_params
-		params.require(:user).permit(:email, :first_name, :last_name, :bio, :location, :password, :password_confirmation, image_attributes: [:picture], skills_attributes: [:id, :name, :done, :_destroy], roles_attributes: [:role_id, :name, :done, :_destroy])
+		params.require(:user).permit(
+			:email,
+			:first_name,
+			:last_name,
+			:bio,
+			:location,
+			:password,
+			:password_confirmation,
+			image_attributes: [
+				:picture
+			],
+			skills_attributes: [
+				:id,
+				:name,
+				:done,
+				:_destroy
+			],
+			roles_attributes: [
+				:id,
+				:name,
+				:done,
+				:_destroy
+			])
 	end
 
 	def load_role
