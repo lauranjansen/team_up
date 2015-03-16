d32e22768264b0616da2928e5975e4beb4782435
Zachary Toth <zachtoth@gmail.com>
2015-03-16 17:26:40 -0400

    Changed the accepting fuctionality and added reject/accept

--- app/controllers/position_requests_controller.rb
+++ app/controllers/position_requests_controller.rb
@@ -1,21 +1,37 @@
 class PositionRequestsController < ApplicationController
-  before_filter :load_project
-
   def new
-    @positionrequest = PositionRequest.new
+    @positionrequest = project.position_requests.new
   end
 
   def create 
-    @position_request = @project.position_requests.build(position_request_params)
+    @position_request = project.position_requests.build(position_request_params)
     @position_request.applicant_id = current_user.id
     if @position_request.save
       redirect_to project_path(@project)
     end
   end
 
+  def accept
+    position_request.accept!
+    redirect_to project_path(project)
+  end
+
+  def reject
+    position_request.reject!
+    redirect_to project_path(project)
+  end
+
   private
   def position_request_params
     params.require(:position_request).permit(:position_id, :project_id)
+  end
+
+  def position_request
+    @position_request ||= project.position_requests.find(params[:id])
+  end
+
+  def project
+    @project ||= Project.find(params[:project_id])
   end
 
   def load_project
--- app/models/position_request.rb
+++ app/models/position_request.rb
@@ -1,4 +1,31 @@
 class PositionRequest < ActiveRecord::Base
 	belongs_to :position
-	belongs_to :user
+	belongs_to :applicant, class_name: "User"
+
+  default_scope {where(status: nil)}
+
+  scope :accepted, ->{where(status: 'accepted')}
+  scope :rejected, ->{where(status: 'rejected')}
+
+  def accept!
+    transaction do
+      position.update_attributes(user: applicant)
+      update_attributes(status: 'accepted')
+      reject_other_applicants if completely_filled?
+    end
+  end
+
+  def reject!
+    update_attributes(status: 'rejected')
+  end
+
+  private
+  def reject_other_applicants
+    position.position_requests.each(&:reject!)
+  end
+
+  def completely_filled?
+    true
+  end
+
 end
