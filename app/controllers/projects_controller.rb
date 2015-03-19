e6815f6a1605b4b00a109e3649b058853f6173c0
saqibddb <saqib.dadabhoy@gmail.com>
2015-03-19 16:59:53 -0400

    Added before_filter require_login for projects#new

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,5 @@
 class ProjectsController < ApplicationController
+   before_filter :require_login, :only => :new
 
   def index
     if params[:search]
--- app/models/project.rb
+++ app/models/project.rb
@@ -4,6 +4,11 @@ class Project < ActiveRecord::Base
 	belongs_to :owner, class_name: 'User'
 	has_many :position_requests, through: :positions
 
+	validates :name, presence: true
+	validates :description, presence: true
+	validates :status, presence: true
+	validates :location, presence: true
+
   paginates_per 6
 
   accepts_nested_attributes_for :positions, reject_if: :all_blank, allow_destroy: true
